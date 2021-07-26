9e1928d069d39ff2924003603bb58c7650e4cfa0
RocioNi <rcno87@gmail.com>
2021-07-26 13:00:45 -0500

    Mostrat imagenes y nombres orden A-Z

--- src/main.js
+++ src/main.js
@@ -1,6 +1,30 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import {info} from './data.js';
 
-console.log(example, data);
+import rickMorty from './data/rickandmorty/rickandmorty.js';
+
+console.log(example, rickMorty);
+
+
+//Importar el filtro de especies para mostrar en consola Humans
+import { speciesFilter } from './data.js';
+console.log(speciesFilter);
+
+//Importar el acomodo de A-Z por nombre
+import { sortName } from './data.js';
+console.log(sortName);
+
+//Para mostrar la imagen y nombre de los personajes
+allCharacters() ;
+    function allCharacters() { //declaramos funcion
+        info.map((objeto)=>{ // creando nvo array a info con map
+          let all = document.createElement('all'); //creando variable all
+          all.classList.add("allchar"); //anade la clase all chart 
+          //creamos constante template para mostrar el nombre del personaje y la imagen
+          const template = `<h1 class='name'> ${objeto.name}</h1> 
+          <img src = ${objeto.image} class = 'image'> </img>`
+          all.innerHTML = template; //remplaza el contenido de all por template
+          document.getElementById('content').appendChild(all); //devuelve elemento content y agrego all a content. Nodo all se adjunta a nodo padre content
+        });
+      }
+  
