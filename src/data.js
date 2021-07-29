836c33c63faf8af36370c2ee56ec2a96e0ce7f08
RocioNi <rcno87@gmail.com>
2021-07-29 18:02:58 -0500

    Se retira codigo no necesario

--- src/data.js
+++ src/data.js
@@ -1,34 +1,13 @@
-import rickMorti from './data/rickandmorty/rickandmorty.js';
+import rickMorty from './data/rickandmorty/rickandmorty.js';
 
 
-export let info= rickMorti.results;
+export let info = rickMorty.results;
 
-//const sortSp = Array.from(info).sort((a, b) => { if (a.species.toLowerCase() < b.species.toLowerCase()) return -1 });
-//export const sortSpecies = info.sort((a, b) => {
-  //if(a.species.toLowerCase() < b.species.toLowerCase()) return -1 });
-//console.log(sortSpecies);
-  
+
 export const sortName = info.sort((a, b) => {
-  if(a.name.toLowerCase() < b.name.toLowerCase()) return -1 });
-//console.log(sortName);
+    if (a.name.toLowerCase() < b.name.toLowerCase()) return -1
+});
 
-//export const speciesFilter= (speciesSelected)=>{
-  // let species= info.filter (kindofspecie=>kindofspecie.species===speciesSelected)
-//return species
-//}
-
-export const speciesFilter= info.filter(function(specieSelected){
-  return specieSelected.species=== "Human";
-
+export const speciesFilter = info.filter(function(specieSelected) {
+    return specieSelected.species === "Human";
 })
-
-export const example = () => {
-  return 'example';
- };
- // estas funciones son de ejemplo
- //export const anotherExample = () => {
- //  return 'OMG';
- //};
- //export const pictures = info.filter((image) => {
-  //return image;
-//});
--- src/main.js
+++ src/main.js
@@ -1,31 +1,21 @@
-import { example } from './data.js';
-import {info} from './data.js';
+import { info, speciesFilter, sortName } from './data.js';
 
-import rickMorty from './data/rickandmorty/rickandmorty.js';
+console.log(speciesFilter, sortName);
 
-console.log(example, rickMorty);
-
-
-//Importar el filtro de especies para mostrar en consola Humans
-import { speciesFilter } from './data.js';
-console.log(speciesFilter);
-
-//Importar el acomodo de A-Z por nombre
-import { sortName } from './data.js';
-console.log(sortName);
 
 //Para mostrar la imagen y nombre de los personajes
-allCharacters() ;
-    function allCharacters() { //declaramos funcion
-        info.map((objeto)=>{ // creando nvo array a info con map
-          let all = document.createElement('all'); //creando variable all
-          all.classList.add("allchar"); //anade la clase all chart 
-          //creamos constante template para mostrar el nombre del personaje y la imagen
-          const template = `<h1 class='name'> Name: ${objeto.name}</h1> <h2 class='gender'>Gender: ${objeto.gender}</h2><h3 class='species'> Specie: ${objeto.species}</h3><h4 class='origin'> Origin: ${objeto.origin.name}</h4>
+allCharacters();
+
+function allCharacters() { //declaramos funcion
+    info.map((objeto) => { // creando nvo array a info con map , cambiamos a for Each
+        let all = document.createElement('all'); //creando variable all
+        all.classList.add("allchar"); //anade la clase all chart 
+        //creamos constante template para mostrar el nombre del personaje y la imagen
+        const template = `<h1 class='name'>Name: ${objeto.name}</h1> <h2 class='gender'>Gender: ${objeto.gender}</h2><h3 class='species'> Specie: ${objeto.species}</h3><h4 class='origin'> Origin: ${objeto.origin.name}</h4>
           <img src = ${objeto.image} class = 'image'> </img> `
-          all.innerHTML = template; //remplaza el contenido de all por template
-          document.getElementById('articles').appendChild(all); //devuelve elemento content y agrego all a content. Nodo all se adjunta a nodo padre content
-          
-        });
-      }
-      
+        all.innerHTML = template; //remplaza el contenido de all por template
+        document.getElementById('articles').appendChild(all); //devuelve elemento content y agrego all a content. Nodo all se adjunta a nodo padre content
+    });
+
+
+}
