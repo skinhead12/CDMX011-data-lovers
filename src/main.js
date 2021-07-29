0134ad0ae7413a073866916161159f12d6a87c8a
Edith Resenos <edithr_isc@hotmail.com>
2021-07-29 16:34:07 -0500

    menu a select

--- src/main.js
+++ src/main.js
@@ -21,10 +21,13 @@ allCharacters() ;
           let all = document.createElement('all'); //creando variable all
           all.classList.add("allchar"); //anade la clase all chart 
           //creamos constante template para mostrar el nombre del personaje y la imagen
-          const template = `<h1 class='name'> Name: ${objeto.name}</h1> <h2 class='gender'>Gender: ${objeto.gender}</h2><h3 class='species'> Specie: ${objeto.species}</h3><h4 class='origin'> Origin: ${objeto.origin.name}</h4>
-          <img src = ${objeto.image} class = 'image'> </img> `
+          const template = `<div class="tarjetas"><h1 class='name'> Name: ${objeto.name}</h1> <h2 class='gender'>Gender: ${objeto.gender}</h2><h3 class='species'> Specie: ${objeto.species}</h3><h4 class='origin'> Origin: ${objeto.origin.name}</h4>
+          <img src = ${objeto.image} class = 'image'> </img></div> `
           all.innerHTML = template; //remplaza el contenido de all por template
           document.getElementById('articles').appendChild(all); //devuelve elemento content y agrego all a content. Nodo all se adjunta a nodo padre content
           
         });
+      return all;
       }
+      
+   
