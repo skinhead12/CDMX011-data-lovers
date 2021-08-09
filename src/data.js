b327bbaf49c26976cec45a4e6f007eab0070df3b
RocioNi <rcno87@gmail.com>
2021-08-09 10:11:53 -0500

    Imprimir info

--- src/data.js
+++ src/data.js
@@ -1,9 +1,90 @@
-// estas funciones son de ejemplo
+import data from './data/rickandmorty/rickandmorty.js';
+
+
+export let info = data.results;
+
+//const sortSp = Array.from(info).sort((a, b) => { if (a.species.toLowerCase() < b.species.toLowerCase()) return -1 });
+//export const sortSpecies = info.sort((a, b) => {
+  //if(a.species.toLowerCase() < b.species.toLowerCase()) return -1 });
+//console.log(sortSpecies);
+  
+//console.log(sortName);
+
+//export const speciesFilter= (speciesSelected)=>{
+  // let species= info.filter (kindofspecie=>kindofspecie.species===speciesSelected)
+//return species
+//}
+
+// export const speciesFilter= info.filter(function(specieSelected){
+//   return specieSelected.species=== "Human";
+
+// })
 
 export const example = () => {
   return 'example';
-};
+ };
+ // estas funciones son de ejemplo
+ //export const anotherExample = () => {
+ //  return 'OMG';
+ //};
+ //export const pictures = info.filter((image) => {
+  //return image;
+//});
+// filtrar por especie 
+export const speciesFilter = (data, tipo) => {
+  let resultSpecies = data.filter(personaje => personaje.species === tipo)
+  return resultSpecies;
+}
+// funcion filtrar por genero
+export const genderFilter = (data, genero) => {
+  let resultGender = data.filter(personaje => personaje.gender === genero)
+  return resultGender;
+  
+}
+// funcion filtrar por episodio
+export const episodesFilter = (data, episodios) => {
+  let resultEpisodes = data.filter(personaje => personaje.episode === episodios)
+ return resultEpisodes;
+}
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const sortFilter = (info) => {
+//   let resultSort = info.sort((a, b) => {
+//   if(a.name.toLowerCase() < b.name.toLowerCase()) return -1  
+//   if (a.name.toLowerCase() > b.name.toLowerCase()) return 1});
+//       return resultSort;   
+// }
+// funcion orden ascendente y descendente 
+  export const sortFilter = (data, seleccion) => {
+      
+    if(seleccion === "upward") {
+      data.sort((a,b) => {
+        a = a.name.toLowerCase();
+        b = b.name.toLowerCase();
+        
+        if(a > b) {
+          return 1;
+        }
+        if(a < b) {
+          return -1;
+        }
+        return 0;
+      
+      });
+      return data;
+    }
+      if(seleccion === "downward") {
+      data.sort((a,b) => {
+        a = a.name.toLowerCase();
+        b = b.name.toLowerCase();
+        
+        if(a > b) {
+          return -1;
+        }
+        if(a < b) {
+          return 1;
+        }
+        return 0;
+      });
+     return data;
+    }
+  }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,61 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import {info, example, speciesFilter, genderFilter, episodesFilter, sortFilter } from './data.js';
+
+import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+console.log(sortFilter); //Importar el acomodo de A-Z por nombre
+console.log(speciesFilter);//Importar el filtro de especies para mostrar en consola Humans
+
+//Para mostrar la imagen y nombre de los personajes
+
+    function allCharacters() { //declaramos funcion
+        info.map((objeto)=>{ // creando nvo array a info con map
+          let all = document.createElement('all'); //creando variable all
+          all.classList.add("allchar"); //anade la clase all chart 
+          //creamos constante template para mostrar el nombre del personaje y la imagen
+          let template = `<div class="tarjetas"><h1 class='name'> Name: ${objeto.name}</h1> <h2 class='gender'>Gender: ${objeto.gender}</h2><h3 class='species'> Specie: ${objeto.species}</h3><h4 class='origin'> Origin: ${objeto.origin.name}</h4>
+          <img src = ${objeto.image} class = 'image'> </img></div> `
+          all.innerHTML = template; //remplaza el contenido de all por template
+          document.getElementById('articles').appendChild(all); //devuelve elemento content y agrego all a content. Nodo all se adjunta a nodo padre content
+       
+      
+      // funcion change en select species
+      
+      document.getElementById("species").addEventListener("change", function(e) {
+        console.log(speciesFilter(info, e.target.value));
+    })
+    
+     // funcion change en select gender
+     document.getElementById("gender").addEventListener("change", function(e) {
+       
+      console.log(genderFilter(info, e.target.value));
+  })
+
+   // funcion change en select episodes
+   document.getElementById("episodes").addEventListener("change", function(e) {
+    console.log(episodesFilter(info, e.target.value));
+})
+
+  // funcion change en select orden az-za
+  document.getElementById('ordenA').addEventListener('change', function(e) {
+    
+    let filtro = (sortFilter(info, e.target.value));
+    let listaOrdenada = '';
+    filtro.forEach((actual) => listaOrdenada += template = actual) ;
+    all.innerHTML = listaOrdenada;
+    //console.log(allCharacters());
+    //console.log(sortFilter(info, e.target.value));
+  })
+});
+        
+}
+allCharacters() ;
+
+  const clear = document.getElementById('clear'); // boton borrar 
+  clear.addEventListener('click', function() {
+    document.getElementById('ordenA').value = '';
+    document.getElementById('species').value = '';
+    document.getElementById('gender').value = '';
+    document.getElementById('episodes').value = '';
+});
