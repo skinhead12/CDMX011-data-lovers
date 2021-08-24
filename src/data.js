76008a5b6c75b23f913abbc8f59ac97c38e3614f
Skinhead12 <edithr_isc@hotmail.com>
2021-08-24 10:01:05 -0500

    ultima modificacion

---
+++ src/data.js
@@ -0,0 +1,58 @@
+import data from './data/rickandmorty/rickandmorty.js';
+
+// funcion de ejemplo
+export let info = data.results;
+
+// usando arrow function
+// filtrar por especie 
+export const speciesFilter = (data, tipo) => {
+  let resultSpecies = data.filter(personaje => personaje.species === tipo)
+  return resultSpecies;
+}
+// funcion filtrar por genero
+export const genderFilter = (data, genero) => {
+  let resultGender = data.filter(personaje => personaje.gender === genero)
+  return resultGender;
+
+}
+// funcion filtrar por episodio
+export const episodesFilter = (data, episodios) => {
+	const resultEpisodes = data.filter((personaje)=>personaje.episode.includes(episodios))
+	return resultEpisodes;
+};
+
+// funcion orden ascendente y descendente 
+  export const sortFilter = (data, seleccion) => {
+
+  if (seleccion === "upward") {
+    data.sort((a, b) => {
+      a = a.name.toLowerCase();
+      b = b.name.toLowerCase();
+
+      if (a > b) {
+        return 1;
+      }
+      if (a < b) {
+        return -1;
+      }
+      return 0;
+
+    });
+    return data;
+  }
+  if (seleccion === "downward") {
+    data.sort((a, b) => {
+      a = a.name.toLowerCase();
+      b = b.name.toLowerCase();
+
+      if (a > b) {
+        return -1;
+      }
+      if (a < b) {
+        return 1;
+      }
+      return 0;
+    });
+    return data;
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,58 @@
+import {info, speciesFilter, genderFilter, episodesFilter, sortFilter } from './data.js';
+
+
+
+//Para mostrar la imagen y nombre de los personajes
+
+function printCharacters(data) { //declaramos funcion
+  let all = ''  // declaramo all vacio ya que se guardaran los datos en esa variable
+  data.forEach((objeto) => { // serealiza un foreach para verificar los datos del arreglo
+          
+          //concatenamos all con el template para mostrar el nombre del personaje y la imagen
+  all += `<div class="tarjetas"><div class="front-card"><img src = ${objeto.image} class="image"> </img><h1 class='name'> ${objeto.name}</h1></div>
+          <div class="back-card"><h2 class="id">Id: ${objeto.id}</h2> <h2 class="status">Status: ${objeto.status}</h2><h2 class="gender">Gender: ${objeto.gender}</h2><h3 class="species"> Specie: ${objeto.species}</h3><h4 class='origin'> Origin: ${objeto.origin.name}</h4></div></div>`
+  });   //
+
+  document.getElementById('articles').innerHTML = all; //devuelve elemento content y agrego all a content. Nodo all se adjunta a nodo padre content
+}
+// meto argumento info en mi funcion allCharacters, ahi cambiare argumento para que guarde los datos nuevos que se le metan 
+printCharacters(info);
+
+// funcion change en select species(especies)
+document.getElementById("species").addEventListener("change", function(e) {
+    let filteredData= speciesFilter(info,e.target.value);
+    console.log(filteredData)
+    printCharacters(filteredData)
+})
+
+// funcion change en select gender(genero)
+document.getElementById("gender").addEventListener("change", function(e) {
+ let filteredData= genderFilter(info,e.target.value);
+console.log(filteredData)
+printCharacters(filteredData)
+})
+
+// funcion change en select episodes(episodios)
+document.getElementById("episodes").addEventListener("change", function(e) {
+    let filteredData= episodesFilter(info,e.target.value);
+    console.log(filteredData)
+    printCharacters(filteredData)
+})
+
+// funcion change en select orden az-za (ascendente y descendente)
+document.getElementById('ordenA').addEventListener('change', function(e) {
+
+  let filteredDataSort = sortFilter(info, e.target.value);
+  console.log(filteredDataSort)
+  printCharacters(filteredDataSort)
+ 
+})
+
+  const clear = document.getElementById('clear'); // boton borrar 
+  clear.addEventListener('click', function() {
+    document.getElementById('ordenA').value = 'Select an option';
+    document.getElementById('species').value = 'Select an option';
+    document.getElementById('gender').value = 'Select an option';
+    document.getElementById('episodes').value = 'Select an option';
+    printCharacters(info);
+});
